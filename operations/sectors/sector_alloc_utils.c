6238ea71e4aeca8d8fe4b4f7c111b9d69855e4d3
filberol <mkh.vadim@gmail.com>
2023-10-04 12:36:32 +0300

    Ref malloc ver 0.2

--- operations/sectors/sector_alloc_utils.c
+++ operations/sectors/sector_alloc_utils.c
@@ -14,25 +14,17 @@ uint32_t allocate_sector(FILE *file, size_t alloc_size) {
     uint32_t header_ptr_free = static_header.first_at_end_free;
     uint32_t last_sector = static_header.last_sector;
 
+    struct SectorHeader header;
+    header.is_taken = true;
+    header.sectors_taken_in_row = alloc_size;
+
     // First allocation
     if (last_sector == TABLE_INDEX_HASH_EMPTY) {
-//        printf("Writing sector first time at %d\n", header_ptr_free);
         // Write sector
-        struct SectorHeader header;
-        header.is_taken = true;
-        header.sectors_taken_in_row = alloc_size;
         header.prev_sector_number = TABLE_INDEX_HASH_EMPTY;
         header.next_sector_number = -1; // Set appropriate values
         write_sector_header_by_index(file, header_ptr_free, &header);
-
-        // Write header
-        static_header.last_sector = header_ptr_free;
-        static_header.first_at_end_free = header_ptr_free + alloc_size;
-        write_static_header(file, &static_header);
-
-        return header_ptr_free;
     } else {
-//        printf("Adding new sector at %d\n", header_ptr_free);
         // Change last sector header
         struct SectorHeader last_header;
         read_sector_header_by_index(file, last_sector, &last_header);
@@ -40,20 +32,17 @@ uint32_t allocate_sector(FILE *file, size_t alloc_size) {
         write_sector_header_by_index(file, last_sector, &last_header);
 
         // Write new sector
-        struct SectorHeader header;
-        header.is_taken = true;
-        header.sectors_taken_in_row = alloc_size;
         header.prev_sector_number = last_sector;
         header.next_sector_number = -1; // Set appropriate values
         write_sector_header_by_index(file, header_ptr_free, &header);
-
-        // Write header
-        static_header.last_sector = header_ptr_free;
-        static_header.first_at_end_free = header_ptr_free + alloc_size;
-        write_static_header(file, &static_header);
-
-        return header_ptr_free;
     }
+
+    // Write header
+    static_header.last_sector = header_ptr_free;
+    static_header.first_at_end_free = header_ptr_free + alloc_size;
+    write_static_header(file, &static_header);
+
+    return header_ptr_free;
 }
 
 void deallocate_sector(FILE *file, uint32_t sector_number) {
